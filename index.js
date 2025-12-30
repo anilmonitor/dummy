fc28f412149b3214257fac98417e98b75d7231e9
ANIL_L <anilarangi6@gmail.com>
2025-12-31 05:25:54 +0530

    some changes

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const finalData = require("./data.json");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
 app.listen(PORT, () => {
   console.log("App is listening on port", PORT);
 });
@@ -11,12 +13,9 @@ app.listen(PORT, () => {
 app.get("/instagram/:username", (req, res) => {
   const { username } = req.params;
   const data = finalData[username];
-  console.log(data);
-  // console.log(data);
 
-  let totalPost = 0;
-  for (let i = 0; i < data.posts.length; i++) {
-    // totalPost = totalPost+1;
+  if (!data) {
+    return res.status(404).send("User not found");
   }
 
   res.render("insta.ejs", { data });
